331b4467f6ab1b5c3d4eb284e7bbdb9b9e13cfd1
Rafa de la Hoz <rafa.delahoz@gmail.com>
2015-10-14 17:48:04 +0200

    Implemented Player jumping and a smooth following camera.

--- source/Entities/Player.hx
+++ source/Entities/Player.hx
@@ -1,26 +1,126 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.util.FlxPoint;
+import flixel.util.FlxSpriteUtil;
 
 class Player extends Entity
 {
 	public var Gravity : Float = 600;
+
+	public var HSpeed : Float = 90;
+	public var VSpeed : Float = 250;
+	
+	public var onAir : Bool;
+	public var canJump : Bool;
+	
+	var jumpHoldTime : Float;
+	var jumpHoldDelta : Float;
+	var maxJumpHold : Float;
+	
+	var display : FlxSprite;
 
 	public function new(X : Float, Y : Float, World : World)
 	{
 		super(X, Y, World);
 
 		makeGraphic(16, 16, 0xFF8AF507);
+		
+		onAir = true;
+		canJump = false;
+		
+		jumpHoldTime = 0;
+		jumpHoldDelta = 1;
+		maxJumpHold = 20;
+		
+		maxVelocity.set(400, 400);
+		
+		display = new FlxSprite(x - 2, y - 4).makeGraphic(20, 4, 0x00000000);
 	}
 
 	override public function update()
-	{
-		acceleration.y = Gravity;
-
-		if (isTouching(FlxObject.DOWN)) {
+	{	
+		onAir = !isTouching(FlxObject.DOWN);
+		
+		if (FlxG.keys.pressed.RIGHT || FlxG.keys.pressed.LEFT)
+		{
+			jumpHoldTime = Math.min(jumpHoldTime + jumpHoldDelta, maxJumpHold);
+		}
+		
+		if (!onAir)
+		{
+			canJump = true;
 			acceleration.y = 0;
+			
+			velocity.set();
+		}
+		else
+		{
+			acceleration.y = Gravity;
+		}
+
+		if (canJump)
+		{
+			if (FlxG.keys.justReleased.RIGHT)
+			{
+				velocity = calculateJumpSpeed(FlxObject.RIGHT);
+				canJump = false;
+				jumpHoldTime = 0;
+			}
+			else if (FlxG.keys.justReleased.LEFT)
+			{
+				velocity = calculateJumpSpeed(FlxObject.LEFT);
+				canJump = false;
+				jumpHoldTime = 0;
+			}
+		}
+		
+		if (!FlxG.keys.anyPressed(["RIGHT", "LEFT"]))
+		{
+			jumpHoldTime = 0;
 		}
 
 		super.update();
+		
+		updateDisplay();
+	}
+	
+	function updateDisplay()
+	{
+		display.x = x - 2;
+		display.y = y - 5;
+		
+		var length : Int = Std.int(jumpHoldTime / maxJumpHold * 20);
+		
+		FlxSpriteUtil.fill(display, 0x00000000);
+		FlxSpriteUtil.drawRect(display, 0, 0, length, 4, 0xFF8AF507);
+		
+		if (FlxG.keys.pressed.RIGHT)
+			FlxSpriteUtil.drawRect(display, 2, 0, 4, 4, 0xFF8888FF);
+		if (FlxG.keys.justReleased.RIGHT)
+			FlxSpriteUtil.drawRect(display, 8, 0, 4, 4, 0xFFFF8888);
+		
+		// display.update();
+	}
+	
+	override public function draw()
+	{
+		super.draw();
+		// display.draw();
+	}
+	
+	function calculateJumpSpeed(direction : Int) : FlxPoint
+	{
+		var speed : FlxPoint = new FlxPoint();
+		
+		var powerBase : Float = jumpHoldTime / maxJumpHold;
+		powerBase = powerBase * 0.5;
+		
+		speed.x = (0.5 + powerBase) * HSpeed * (direction == FlxObject.LEFT ? -1 : 1);
+		speed.y = -(0.5 + powerBase) * VSpeed;
+		
+		return speed;
 	}
 }
--- source/World.hx
+++ source/World.hx
@@ -3,6 +3,8 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.FlxCamera;
+
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
@@ -17,16 +19,26 @@ class World extends FlxState
 	override public function create():Void
 	{
 		super.create();
-
+		
+		FlxG.scaleMode = new flixel.system.scaleModes.PixelPerfectScaleMode();
+		
 		player = new Player(FlxG.width / 2, FlxG.height / 2, this);
 		add(player);
+		
+		FlxG.watch.add(player, "x");
+		FlxG.watch.add(player, "y");
+		FlxG.watch.add(FlxG, "worldBounds");
 
-		var g : FlxSprite = new flixel.FlxSprite(0, FlxG.height - 48).makeGraphic(FlxG.width, 48, 0xFF000000);
-
+		var g : FlxSprite = new flixel.FlxSprite(0, FlxG.height - 48).makeGraphic(4000, 48, 0x00141114);
+		g.immovable = true;
+		
 		ground = new FlxGroup();
 		ground.add(g);
 
 		add(ground);
+		
+		FlxG.camera.setBounds(0, 0, 4000, FlxG.height + 48, true);
+		FlxG.camera.follow(player, FlxCamera.STYLE_PLATFORMER, null, 14);
 	}
 	
 	override public function destroy():Void
